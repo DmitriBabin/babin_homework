c16964b3659477c95444bf35209054360d0797ab
DmitriBabin <70965349+DmitriBabin@users.noreply.github.com>
2020-11-21 23:33:13 +0300

    Update euclidian_algorithm.py

--- euclidian_algorithm.py
+++ euclidian_algorithm.py
@@ -1,14 +1,14 @@
 import random as rd
 import math
 
-a = rd.randint(1, 100)
-b = rd.randint(1, 100)
-print('gcd(',a,',',b,')')
-print('Значение, полученное встроенным алгоритмом = ', math.gcd(a,b))
-def gcd(a,b):
-    if b == 0:
-        return a
+a_number = rd.randint(1, 100)
+b_number = rd.randint(1, 100)
+print('gcd(',a_number,',',b_number,')')
+print('Значение, полученное встроенным алгоритмом = ', math.gcd(a_number,b_number))
+def gcd(a_number,b_number):
+    if b_number == 0:
+        return a_number
     else:
-        return gcd(b, a % b)
+        return gcd(b_number, a_number % b_number)
 
-print('Значение, полученное встроенным алгоритмом = ', gcd(a,b))
+print('Значение, полученное встроенным алгоритмом = ', gcd(a_number,b_number))
