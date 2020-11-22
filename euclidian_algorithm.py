cabbbe675de62f18076d78da37e5cf4a5963dd48
DmitriBabin <70965349+DmitriBabin@users.noreply.github.com>
2020-11-22 22:57:03 +0300

    Update euclidian_algorithm.py

--- euclidian_algorithm.py
+++ euclidian_algorithm.py
@@ -12,3 +12,12 @@ def gcd(a_number,b_number):
         return gcd(b_number, a_number % b_number)
 
 print('Значение, полученное встроенным алгоритмом = ', gcd(a_number,b_number))
+
+def extended_gcd(a_number,b_number):
+    if a_number == 0:
+        return (0, 1)
+    else:
+        x_num , y_num = extended_gcd(b_number % a_number, a_number)
+    return ( y_num - (b_number // a_number) * x_num, x_num)
+print(a_number, '*', extended_gcd(a_number,b_number)[0], '+', b_number,
+      '*', extended_gcd(a_number,b_number)[1], '=' , gcd(a_number,b_number))
