0748e901a77019ef58e4dd07f2c90ac725c23ca7
DmitriBabin <70965349+DmitriBabin@users.noreply.github.com>
2020-12-22 23:13:39 +0300

    Update factorial_sequence.py

--- factorial_sequence.py
+++ factorial_sequence.py
@@ -1,21 +1,15 @@
 import math
 import itertools as it
 
-def make_inf_sequence():
-    x_num = 0
-    while True:
-        yield x_num
-        x_num = x_num + 1
 class Factorial:
     class Factorialiter:
         def __init__(self):
-            self.i = 0
-            self.fact = make_inf_sequence()
+            self.i = 1
+            self.z = 1
         def __next__(self):
-            for self.i in self.fact:
-                a_num = math.factorial(self.i)
-                self.i += 1
-                return a_num
+            self.z *= self.i
+            self.i += 1
+            return self.z
 
     def __iter__(self):
         return Factorial.Factorialiter()
