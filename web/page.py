65e4b1d9e31aab72f32489291f89252a59c68bf8
DmitriBabin <70965349+DmitriBabin@users.noreply.github.com>
2021-04-26 12:03:38 +0300

    Update page.py

--- web/page.py
+++ web/page.py
@@ -29,8 +29,6 @@ def vector_gauss(ab_vect):
     x = ab_vect[:, -1]
     return x
 
-a_num = rd.randint(1, 100)
-b_num = rd.randint(1, 100)
 
 def gcd(a_number,b_number):
     if b_number == 0:
@@ -47,28 +45,54 @@ def extended_gcd(a_number,b_number):
 
 app = Flask(__name__, static_folder="static", static_url_path="", template_folder="templates")
 
-@app.route('/')
+@app.route('/', methods = ['GET','POST'])
 def root():
     return flask.render_template(
         'base.html'
     )
 
+
 @app.route('/gauss', methods = ['GET','POST'])
 def gauss():
     if request.method == 'POST':
-        task_1 = vector_gauss(ab_vect)
-        return flask.render_template(
-        'gauss.html',
-        vect_1 = ab_vect[0],
-        vect_2 = ab_vect[1],
-        vect_3 = ab_vect[2],
-        answer_g = task_1
+        ab_vect[0,0] = request.form.get('a1', type=int)
+        ab_vect[0,1] = request.form.get('a2', type=int)
+        ab_vect[0,2] = request.form.get('a3', type=int)
+        ab_vect[0,3] = request.form.get('a4', type=int)
+        ab_vect[0,4] = request.form.get('a5', type=int)
+        ab_vect[1,0] = request.form.get('b1', type=int)
+        ab_vect[1,1] = request.form.get('b2', type=int)
+        ab_vect[1,2] = request.form.get('b3', type=int)
+        ab_vect[1,3] = request.form.get('b4', type=int)
+        ab_vect[1,4] = request.form.get('b5', type=int)
+        ab_vect[2,0] = request.form.get('c1', type=int)
+        ab_vect[2,1] = request.form.get('c2', type=int)
+        ab_vect[2,2] = request.form.get('c3', type=int)
+        ab_vect[2,3] = request.form.get('c4', type=int)
+        ab_vect[2,4] = request.form.get('c5', type=int)
+        ab_vect[3,0] = request.form.get('d1', type=int)
+        ab_vect[3,1] = request.form.get('d2', type=int)
+        ab_vect[3,2] = request.form.get('d3', type=int)
+        ab_vect[3,3] = request.form.get('d4', type=int)
+        ab_vect[3,4] = request.form.get('d5', type=int)
+    task_1 = vector_gauss(ab_vect)
+    return flask.render_template(
+    'gauss.html',
+    vect_1 = ab_vect[0],
+    vect_2 = ab_vect[1],
+    vect_3 = ab_vect[2],
+    answer_g = task_1
     )
 
 
 @app.route('/euclid', methods = ['GET','POST'])
 def euclid():
-   if request.method == 'POST':
+    if request.method == 'POST':
+        a_num=request.form.get('a_num', type=int)
+        b_num=request.form.get('b_num', type=int)
+    else:
+        a_num=rd.randint(1,100)
+        b_num=rd.randint(1,100)
     short = gcd(a_num, b_num)
     pro = extended_gcd(a_num,b_num)
     return flask.render_template(
